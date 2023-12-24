d1ebffa60ee13e8b513af44608d9b29a2dfedd39
venkateshh137 <venkateshhkaruparthi@gmail.com>
2023-12-24 16:03:53 +0530

    Initial Commit

---
+++ src/main/java/arrays/ArraysTopic.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class ArraysTopic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello welcome to DSA!!!!!");
+	}
+
+}
